323c7751548a2354bf4137e96742e25578143a87
Manuel Perez Regalado <manuel@leanmind.es>
2021-10-22 12:02:27 +0100

    🚧 add OCP example

---
+++ presentation/OCP/calculateAreas.ts
@@ -0,0 +1,64 @@
+interface Circle {
+  radius: number
+}
+
+interface Rectangle {
+  width: number
+  heigth: number
+}
+
+class AreaCalculator {
+  sumRectangleAreas(rectangles: Rectangle[]): number {
+    const totalArea = 0
+
+    for(const rectangle of rectangles) {
+      totalArea += (rectangle.width * rectangle.heigth)
+    }
+
+    return totalArea
+  }
+
+  sumCircleAreas(circles: Circle[]): number {
+    const totalArea = 0
+
+    for(const circle of circles) {
+      totalArea += (circle.radius * Math.pow(Math.PI, 2))
+    }
+
+    return totalArea
+  }
+}
+
+
+interface Shape {
+  calculateArea(): number
+}
+
+class Circle implements Shape {
+  radius: number
+
+  calculateArea(): number {
+    return (this.radius * Math.pow(Math.PI, 2))
+  }
+}
+
+class Rectangle implements Shape {
+  width: number
+  heigth: number
+
+  calculateArea(): number {
+    return this.width * this.heigth
+  }
+}
+
+class AreaCalculator {
+  sumAreas(shapes: Shapes[]): number {
+    const totalArea = 0
+
+    for(const shape of shapes) {
+      totalArea += shape.calculateArea()
+    }
+
+    return totalArea
+  }
+}
